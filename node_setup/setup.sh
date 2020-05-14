0ebddb6dfcc172b425097e8ca0fa0961d4f9b1d1
Dennis Tsvetkov <dennis.tsvetkov@gmail.com>
2020-05-15 00:07:21 +0300

    README updated

--- node_setup/setup.sh
+++ node_setup/setup.sh
@@ -16,4 +16,8 @@ apt install -y \
 # fix sudoers
 sed -i "s/%sudo\tALL=(ALL:ALL) ALL/%sudo\tALL=(ALL:ALL) NOPASSWD:ALL/" /etc/sudoers
 
+# fix .bashrc
+sed -i "s/#alias ll='ls -l'/alias ll='ls -lah'/" /etc/skel/.bashrc
+cp -f /etc/skel/.bashrc /root/.bashrc
+. /root/.bashrc
 
