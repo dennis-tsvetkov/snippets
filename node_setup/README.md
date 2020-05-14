a2fd4eacd8f86ecde8d024dfc7a425f933bcd838
Dennis Tsvetkov <dennis.tsvetkov@gmail.com>
2020-05-15 00:19:18 +0300

    setup.sh updated

--- node_setup/README.md
+++ node_setup/README.md
@@ -1,5 +1,6 @@
 run as root:
 ```
 wget -q -O - https://raw.githubusercontent.com/dennis-tsvetkov/snippets/master/node_setup/setup.sh | bash
+. ~/.bashrc
 
 ```
--- node_setup/setup.sh
+++ node_setup/setup.sh
@@ -11,6 +11,10 @@ apt install -y \
   htop \
   atop \
   screen \
+  net-tools \
+  iotop \
+  iftop \
+  ntop \
   tmux \
   git
 
@@ -18,7 +22,9 @@ apt install -y \
 sed -i "s/%sudo\tALL=(ALL:ALL) ALL/%sudo\tALL=(ALL:ALL) NOPASSWD:ALL/" /etc/sudoers
 
 # fix .bashrc
+echo 'export PS1="\u@\h \[\033[1;34m\]\w\[\033[0;33m\]\$(parse_git_branch)\[\033[00m\] $ "' >> /etc/skel/.bashrc
+echo 'shopt -s histappend' >> /etc/skel/.bashrc
+
 sed -i "s/#alias ll='ls -l'/alias ll='ls -lah'/" /etc/skel/.bashrc
 cp -f /etc/skel/.bashrc /root/.bashrc
-source /root/.bashrc
 
